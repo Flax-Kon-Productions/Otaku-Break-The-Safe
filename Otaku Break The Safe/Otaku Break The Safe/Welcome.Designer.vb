2eb357e7600dc84d0968dda8b23fa1a31db9f57a
Jacob Flax <flax_jacob@hotmail.com>
2020-01-04 21:44:11 -0600

    First Window.

---
+++ Otaku Break The Safe/Otaku Break The Safe/Welcome.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Welcome
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LoadPlayers = New System.Windows.Forms.Button()
+        Me.topMenu = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.topMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Otaku_Break_The_Safe.My.Resources.Resources.Show_logo
+        Me.PictureBox1.Location = New System.Drawing.Point(49, 42)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(496, 386)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'LoadPlayers
+        '
+        Me.LoadPlayers.Location = New System.Drawing.Point(575, 42)
+        Me.LoadPlayers.Name = "LoadPlayers"
+        Me.LoadPlayers.Size = New System.Drawing.Size(201, 386)
+        Me.LoadPlayers.TabIndex = 1
+        Me.LoadPlayers.Text = "Load Players"
+        Me.LoadPlayers.UseVisualStyleBackColor = True
+        '
+        'topMenu
+        '
+        Me.topMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.topMenu.Location = New System.Drawing.Point(0, 0)
+        Me.topMenu.Name = "topMenu"
+        Me.topMenu.Size = New System.Drawing.Size(800, 24)
+        Me.topMenu.TabIndex = 2
+        Me.topMenu.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'Welcome
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LoadPlayers)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.topMenu)
+        Me.MainMenuStrip = Me.topMenu
+        Me.Name = "Welcome"
+        Me.Text = "Welcome"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.topMenu.ResumeLayout(False)
+        Me.topMenu.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LoadPlayers As Button
+    Friend WithEvents topMenu As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+End Class
